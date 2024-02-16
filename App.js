c8003769ccf66de3bf0556b734aba7a90d09ebd2
Jimena Agustín <142707570+JimenaAgustinNavarro@users.noreply.github.com>
2024-02-16 13:01:50 -0500

    cambioss

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Inicio from './vista/inicio.js'
 
-export default function App() {
+import Inicio from './vista/inicio';
+
+const Stack = createStackNavigator()
+function App() {
+  
   return (
-    <View>
-      <Inicio></Inicio>
-    </View> 
-  );
-}
+    <NavigationContainer>
+      
+        
+        <Inicio/>
+    
+    </NavigationContainer>
+  )
+  }
+export default App
--- vista/inicio.js
+++ vista/inicio.js
@@ -1,70 +1,77 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+    import React from 'react'
+    import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-const TuComponente = () => {
-  return (
-    <View style={styles.container}>
-      <View style={[styles.section, styles.redSection]}>
-        <Text style={styles.text}>App de Ventas</Text>
-        <Text style={[styles.text, styles.textod]}>Tu Sitio Ideal</Text> 
-    </View>
 
-      <View style={[styles.section, styles.blackSection]}>
-      <View style={{ marginTop: 50}}>
-      <TouchableOpacity style={[styles.button, { marginBottom: 10 }]}>
-          <Text style={styles.buttonText}>Iniciar Sesion</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}> Registrarse </Text>
-        </TouchableOpacity>
-      </View>
-      </View>
-    </View>
-  );
-};
+    function Inicio(navigate) {
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column', // Esto asegura que los elementos se coloquen de forma vertical
-  },
-    section: {
-    borderColor: '#ccc',
-    width: '100%',
-    alignItems: 'center',
-  },
-    redSection: {
-     backgroundColor: 'red', // Fondo rojo para la primera sección
-     paddingVertical: '55%',
-     justifyContent: 'center',
-  },
-    
-  text: {
-    fontSize: 50,
-    fontWeight: 'bold',
-    color: 'white',
-    top:'70%',
-  },
-  textod:{
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'white',
-  }, 
-  button: {
-    backgroundColor: 'red',
-    padding:'1%',
-    borderRadius: 8,
-    height:'25%',
-    width: '100%',
-    justifyContent: 'space-between',
-    paddingStart:'55%',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-    height:50,
-    right:'60%',
-    top:'30%',
-  },
-});
+    return (
+        <View style={styles.container}>
+        <View>
+            <Image 
+            source={{ url: 'url ',}}/>
+        </View>
+        <View style={[styles.section, styles.redSection]}>
+            <Text style={styles.text}>App de Ventas</Text>
+            <Text style={[styles.text, styles.textod]}>Tu Sitio Ideal</Text> 
+        </View>
+        <View style={[styles.section, styles.blackSection]}>
+        <View style={{ marginTop: 50}}>
+        <TouchableOpacity style={[styles.button, { marginBottom: 10 }]}
+        onPress={() => console.log('pressed')} >
+            <Text style={styles.buttonText}>Iniciar Sesion</Text>
+            </TouchableOpacity> 
+            <TouchableOpacity style={styles.button}
+            onPress={() => navigation.navigate("Registro")}>
+            <Text style={styles.buttonText}> Registrarse </Text>
+            </TouchableOpacity>
 
-export default TuComponente;
+        </View>
+        </View>
+        </View>
+    );
+    };
+
+    const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column', // Esto asegura que los elementos se coloquen de forma vertical
+    },
+        section: {
+        borderColor: '#ccc',
+        width: '100%',
+        alignItems: 'center',
+    },
+        redSection: {
+        backgroundColor: 'red', // Fondo rojo para la primera sección
+        paddingVertical: '55%',
+        justifyContent: 'center',
+    },
+        
+    text: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        color: 'white',
+        top:'70%',
+    },
+    textod:{
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: 'white',
+    }, 
+    button: {
+        backgroundColor: 'red',
+        padding:'auto',
+        borderRadius: 8,
+        height:'25%',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingStart:'55%',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 20,
+        height:50,
+        right:'60%',
+        top:'30%',
+    },
+    });
+    export default Inicio;
